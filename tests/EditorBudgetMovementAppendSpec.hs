815f0bf8f31aadff28ad96d5411ef2c30e0595d2
shumoku88-bit <shumoku88@gmail.com>
2026-08-05 07:41:44 +0900

    test(editor): commit Budget source with its TSV admission

--- tests/EditorBudgetMovementAppendSpec.hs
+++ tests/EditorBudgetMovementAppendSpec.hs
@@ -1,22 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main (main) where
 
+import Control.Exception (IOException, catch)
 import Data.Text (Text)
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
 import Data.Time.Calendar (fromGregorian)
+import System.Directory (removeFile)
 import System.Exit (exitFailure, exitSuccess)
 
 import HKernel.Account (mkAccount)
-import HKernel.Money (mkAmount, mkCommodity, quantityFromInteger)
+import HKernel.Editor.ActualWriter (WriteIntent(..), publishWithAdmission)
+import HKernel.Editor.BudgetMovementAppend
+  ( BudgetMovementAppendPreview(..)
+  , prepareBudgetMovementAppend
+  )
 import HKernel.Household.BudgetMovement (HouseholdBudgetMovement(..))
-import HKernel.Editor.BudgetMovementAppend (prepareBudgetMovementAppend, candidateBlock, BudgetMovementAppendPreview)
+import HKernel.Household.BudgetMovement.TSV
+  ( parseHouseholdBudgetMovements )
+import HKernel.Money (mkAmount, mkCommodity, quantityFromInteger)
 
 main :: IO ()
 main = do
-  let results = [ ("testValidBudgetMovement", testValidBudgetMovement) ]
-  mapM_ print results
-  if all snd results
+  let tests =
+        [ ("testValidBudgetMovement", pure testValidBudgetMovement)
+        , ("testBudgetMovementCommit", testBudgetMovementCommit)
+        ]
+  results <- sequence [action | (_, action) <- tests]
+  let namedResults = zip (map fst tests) results
+  mapM_ print namedResults
+  if all snd namedResults
     then exitSuccess
     else exitFailure
 
@@ -27,18 +42,54 @@ fixtureSource = T.unlines
 
 testMovement :: HouseholdBudgetMovement
 testMovement = HouseholdBudgetMovement
-  { householdBudgetMovementDate   = fromGregorian 2026 8 4
-  , householdBudgetMovementMemo   = "transfer"
-  , householdBudgetMovementFrom   = either (error "bad from") id (mkAccount "budget:food")
-  , householdBudgetMovementTo     = either (error "bad to") id (mkAccount "budget:living")
-  , householdBudgetMovementAmount = mkAmount (either (error "bad comm") id (mkCommodity "JPY")) (quantityFromInteger 500)
+  { householdBudgetMovementDate = fromGregorian 2026 8 4
+  , householdBudgetMovementMemo = "transfer"
+  , householdBudgetMovementFrom =
+      either (error "bad from") id (mkAccount "budget:food")
+  , householdBudgetMovementTo =
+      either (error "bad to") id (mkAccount "budget:living")
+  , householdBudgetMovementAmount =
+      mkAmount
+        (either (error "bad comm") id (mkCommodity "JPY"))
+        (quantityFromInteger 500)
   }
 
 testValidBudgetMovement :: Bool
 testValidBudgetMovement =
-  let result = prepareBudgetMovementAppend fixtureSource testMovement
-  in case result of
-       Right preview ->
-         let block = candidateBlock preview
-         in "2026-08-04\ttransfer\tbudget:food\tbudget:living\t500\tcurrency=JPY" == block
-       Left err -> error (show err)
+  case prepareBudgetMovementAppend fixtureSource testMovement of
+    Right preview ->
+      candidateBlock preview
+        == "2026-08-04\ttransfer\tbudget:food\tbudget:living\t500\tcurrency=JPY"
+    Left err -> error (show err)
+
+testBudgetMovementCommit :: IO Bool
+testBudgetMovementCommit = do
+  let path = "tests/fixtures/test_editor_budget_commit.tsv"
+  cleanup path
+  TIO.writeFile path fixtureSource
+  result <- case prepareBudgetMovementAppend fixtureSource testMovement of
+    Left err -> print err >> pure False
+    Right preview -> do
+      writeResult <- publishWithAdmission
+        parseHouseholdBudgetMovements
+        WriteIntent
+          { targetFilePath = path
+          , expectedOldBytes = fixtureSource
+          , candidateNewBytes = candidateCompleteSource preview
+          }
+      case writeResult of
+        Left err -> print err >> pure False
+        Right () ->
+          (== candidateCompleteSource preview) <$> TIO.readFile path
+  cleanup path
+  pure result
+
+cleanup :: FilePath -> IO ()
+cleanup path = do
+  removeIfPresent path
+  removeIfPresent (path <> ".backup.tmp")
+  removeIfPresent (path <> ".new.tmp")
+
+removeIfPresent :: FilePath -> IO ()
+removeIfPresent path =
+  catch (removeFile path) (\(_ :: IOException) -> pure ())
