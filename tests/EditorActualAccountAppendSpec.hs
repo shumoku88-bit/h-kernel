52c11b7c865c653e79b50c24258194f9c258dbeb
shumoku88-bit <shumoku88@gmail.com>
2026-08-05 12:22:14 +0900

    test(editor): cover Account declaration identity parity

--- tests/EditorActualAccountAppendSpec.hs
+++ tests/EditorActualAccountAppendSpec.hs
@@ -7,16 +7,32 @@ import Data.Text (Text)
 import qualified Data.Text as T
 import System.Exit (exitFailure, exitSuccess)
 
-import HKernel.Account (Account, mkAccount, AccountType(..), declareAccount, declareAccountWithDefaultCommodity)
-import HKernel.Money (Commodity, mkCommodity)
+import HKernel.Account
+  ( Account
+  , AccountType(..)
+  , declareAccount
+  , declareAccountWithDefaultCommodity
+  , lookupAccountDeclaration
+  , mkAccount
+  )
+import HKernel.Account.Journal (AccountDeclarationRenderError(..))
+import HKernel.Actual.Journal
+  ( actualJournalValue
+  , parseActualJournal
+  )
 import HKernel.Editor.ActualAccountAppend
+import HKernel.Journal (journalAccountRegistry)
+import HKernel.Money (Commodity, mkCommodity)
 
 main :: IO ()
 main = do
-  let results = [ ("testValidAccountNoCommodity", testValidAccountNoCommodity)
-                , ("testValidAccountWithCommodity", testValidAccountWithCommodity)
-                , ("testDuplicateAccount", testDuplicateAccount)
-                ]
+  let results =
+        [ ("testValidAccountNoCommodity", testValidAccountNoCommodity)
+        , ("testValidAccountWithCommodity", testValidAccountWithCommodity)
+        , ("testDuplicateAccount", testDuplicateAccount)
+        , ("testAllAccountTypesRoundTrip", testAllAccountTypesRoundTrip)
+        , ("testCommentDelimiterRejected", testCommentDelimiterRejected)
+        ]
   mapM_ print results
   if all snd results
     then exitSuccess
@@ -37,38 +53,77 @@ fixtureSource = T.unlines
   ]
 
 accNew :: Account
-accNew = either (error "bad account") id (mkAccount "expenses:food")
+accNew = mustAccount "expenses:food"
 
 accExisting :: Account
-accExisting = either (error "bad account") id (mkAccount "assets:cash")
+accExisting = mustAccount "assets:cash"
 
 commJPY :: Commodity
 commJPY = either (error "bad comm") id (mkCommodity "JPY")
 
 testValidAccountNoCommodity :: Bool
 testValidAccountNoCommodity =
-  let intent = declareAccount accNew Expense
-      result = prepareActualAccountAppend fixtureSource intent
+  let declaration = declareAccount accNew Expense
+      result = prepareActualAccountAppend fixtureSource declaration
   in case result of
        Right preview ->
-         let block = candidateBlock preview
-         in "account expenses:food\n  type: Expense\n" == block
+         "account expenses:food\n  type: Expense\n"
+           == candidateBlock preview
        Left err -> error (show err)
 
 testValidAccountWithCommodity :: Bool
 testValidAccountWithCommodity =
-  let intent = declareAccountWithDefaultCommodity accNew Expense commJPY
-      result = prepareActualAccountAppend fixtureSource intent
+  let declaration = declareAccountWithDefaultCommodity accNew Expense commJPY
+      result = prepareActualAccountAppend fixtureSource declaration
   in case result of
        Right preview ->
-         let block = candidateBlock preview
-         in "account expenses:food\n  type: Expense\n  commodity: JPY\n" == block
+         "account expenses:food\n  type: Expense\n  commodity: JPY\n"
+           == candidateBlock preview
        Left err -> error (show err)
 
 testDuplicateAccount :: Bool
 testDuplicateAccount =
-  let intent = declareAccount accExisting Asset
-      result = prepareActualAccountAppend fixtureSource intent
+  let declaration = declareAccount accExisting Asset
+      result = prepareActualAccountAppend fixtureSource declaration
   in case result of
-       Left (DuplicateDeclaration acc :| []) -> acc == accExisting
+       Left (DuplicateDeclaration account :| []) -> account == accExisting
        _ -> False
+
+testAllAccountTypesRoundTrip :: Bool
+testAllAccountTypesRoundTrip =
+  all declarationRoundTrips
+    [ Asset
+    , Liability
+    , Equity
+    , Income
+    , Expense
+    , Budget
+    ]
+  where
+    declarationRoundTrips accountType =
+      let account = mustAccount
+            ("roundtrip:" <> T.toCaseFold (T.pack (show accountType)))
+          declaration =
+            declareAccountWithDefaultCommodity account accountType commJPY
+      in case prepareActualAccountAppend fixtureSource declaration of
+           Left _ -> False
+           Right preview ->
+             case parseActualJournal (candidateCompleteSource preview) of
+               Left _ -> False
+               Right journal ->
+                 lookupAccountDeclaration
+                   account
+                   (journalAccountRegistry (actualJournalValue journal))
+                   == Just declaration
+
+testCommentDelimiterRejected :: Bool
+testCommentDelimiterRejected =
+  let account = mustAccount "expenses:food;legacy"
+      declaration = declareAccount account Expense
+  in case prepareActualAccountAppend fixtureSource declaration of
+       Left (DeclarationRenderError AccountDeclarationUnrepresentable :| []) ->
+         True
+       _ -> False
+
+mustAccount :: Text -> Account
+mustAccount value = either (error "bad account") id (mkAccount value)
