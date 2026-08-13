84de68ba9bdee17fe48d0941a7bd2a817cbeb734
shumoku88-bit <shumoku88@gmail.com>
2026-08-13 11:33:32 +0900

    test(actual): qualify optional identity promotion

--- tests/EditorActualWorkspaceSpec.hs
+++ tests/EditorActualWorkspaceSpec.hs
@@ -24,14 +24,20 @@ import HKernel.Actual.Journal
   , parseActualJournal
   )
 import HKernel.Editor.ActualWorkspace
-  ( newestTransactionEntriesForAccount
+  ( ActualIdentityPromotionError(..)
+  , identityPromotionCandidateCompleteSource
+  , newestTransactionEntriesForAccount
+  , prepareActualIdentityPromotion
   , transactionEntriesForAccount
   )
 import HKernel.Journal
   ( journalAccountRegistry
   , journalTransactionSourceHeaderLine
   )
-import HKernel.Plan.Completion (actualTransactionIdText)
+import HKernel.Plan.Completion
+  ( actualTransactionIdText
+  , mkActualTransactionId
+  )
 
 main :: IO ()
 main = do
@@ -58,6 +64,7 @@ main = do
 
   let alignedJournal = mustRight (parseActualJournal duplicateShapeSource)
       alignedEntries = actualJournalTransactionEntries alignedJournal
+      newestEntries = newestTransactionEntriesForAccount Nothing alignedEntries
       alignmentResult =
         ( "source-position identity survives duplicate Transaction values"
         , map (fmap actualTransactionIdText . actualTransactionEntryIdentity)
@@ -74,11 +81,11 @@ main = do
       newestFirstResult =
         ( "newest-first projection reverses display without changing source entries"
         , map (fmap actualTransactionIdText . actualTransactionEntryIdentity)
-            (newestTransactionEntriesForAccount Nothing alignedEntries)
+            newestEntries
             == [Just "actual-second", Nothing]
             && map
                 (journalTransactionSourceHeaderLine . actualTransactionEntrySource)
-                (newestTransactionEntriesForAccount Nothing alignedEntries)
+                newestEntries
               == [13, 9]
             && map (fmap actualTransactionIdText . actualTransactionEntryIdentity)
                 alignedEntries
@@ -88,7 +95,55 @@ main = do
                 alignedEntries
               == [9, 13]
         )
-      results = fixtureResults ++ [alignmentResult, sourceEvidenceResult, newestFirstResult]
+      promotedId = mustRight (mkActualTransactionId "actual-first")
+      displayedIdentityFree = last newestEntries
+      promotionPreview = mustRight
+        (prepareActualIdentityPromotion
+          alignedJournal
+          duplicateShapeSource
+          displayedIdentityFree
+          promotedId)
+      promotedSource = identityPromotionCandidateCompleteSource promotionPreview
+      promotedJournal = mustRight (parseActualJournal promotedSource)
+      promotionResult =
+        ( "identity promotion targets selected source evidence without changing accounting meaning"
+        , actualJournalValue promotedJournal == actualJournalValue alignedJournal
+            && map (fmap actualTransactionIdText . actualTransactionEntryIdentity)
+                (actualJournalTransactionEntries promotedJournal)
+              == [Just "actual-first", Just "actual-second"]
+            && "2026-08-08 Same transaction\n  ; event-id: actual-first\n  assets:cash"
+                `T.isInfixOf` promotedSource
+        )
+      alreadyIdentifiedResult =
+        ( "identity promotion rejects an already identified selected Actual"
+        , case prepareActualIdentityPromotion
+            alignedJournal
+            duplicateShapeSource
+            (head newestEntries)
+            promotedId of
+            Left (ActualIdentityPromotionAlreadyIdentified existingId) ->
+              actualTransactionIdText existingId == "actual-second"
+            _ -> False
+        )
+      duplicateIdentityResult =
+        ( "identity promotion rejects collision with an existing durable Actual identity"
+        , case prepareActualIdentityPromotion
+            alignedJournal
+            duplicateShapeSource
+            displayedIdentityFree
+            (mustRight (mkActualTransactionId "actual-second")) of
+            Left (ActualIdentityPromotionIdAlreadyExists existingId) ->
+              actualTransactionIdText existingId == "actual-second"
+            _ -> False
+        )
+      results = fixtureResults ++
+        [ alignmentResult
+        , sourceEvidenceResult
+        , newestFirstResult
+        , promotionResult
+        , alreadyIdentifiedResult
+        , duplicateIdentityResult
+        ]
 
   mapM_ print results
   if all snd results then exitSuccess else exitFailure
