0548f47442650ce0ae1e8cb4cedd58dba2a3e08d
shumoku88-bit <shumoku88@gmail.com>
2026-08-16 21:01:40 +0900

    test(issue): make issue spec importable

--- tests/HouseholdIssueSpec.hs
+++ tests/HouseholdIssueSpec.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Main (main) where
+module HouseholdIssueSpec (main) where
 
 import Test.Support (mustRight, assertEqual)
 import qualified Data.List.NonEmpty as NonEmpty
