37d6b5295df550d15a587f4d37fa14b399cdaa8a
shumoku88-bit <shumoku88@gmail.com>
2026-08-16 20:43:31 +0900

    test(envelope): add semantic test runner

---
+++ tests/EnvelopeSpec.hs
@@ -0,0 +1,23 @@
+module Main (main) where
+
+import qualified EnvelopeBudgetSpec
+import qualified EnvelopeCommitmentHeadroomSpec
+import qualified EnvelopeConsumptionSpec
+import qualified EnvelopeEntitlementHistorySpec
+import qualified EnvelopeEntitlementSpec
+import qualified EnvelopeExpenseRoutingSpec
+import qualified EnvelopeFulfillmentRoutingTSVSpec
+import qualified EnvelopeFulfillmentSpec
+import qualified EnvelopeRemainingSpec
+
+main :: IO ()
+main = do
+  EnvelopeCommitmentHeadroomSpec.main
+  EnvelopeConsumptionSpec.main
+  EnvelopeExpenseRoutingSpec.main
+  EnvelopeFulfillmentSpec.main
+  EnvelopeFulfillmentRoutingTSVSpec.main
+  EnvelopeEntitlementSpec.main
+  EnvelopeEntitlementHistorySpec.main
+  EnvelopeRemainingSpec.main
+  EnvelopeBudgetSpec.main
