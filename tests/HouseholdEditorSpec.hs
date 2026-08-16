9ace7e8f77440f438e1954fd789d1d08ae5bf951
shumoku88-bit <shumoku88@gmail.com>
2026-08-16 21:02:45 +0900

    test(editor): remove redundant workspace runner

--- tests/HouseholdEditorSpec.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-
-import qualified HouseholdIssueSpec
-import qualified HouseholdWorkspaceProjectionSpec
-
-main :: IO ()
-main = do
-  HouseholdIssueSpec.main
-  HouseholdWorkspaceProjectionSpec.main
