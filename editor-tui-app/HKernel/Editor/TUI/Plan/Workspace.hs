62a1a81412147583fd3018e9e3a288f76cd67e3e
shumoku88-bit <shumoku88@gmail.com>
2026-08-19 19:25:11 +0900

    refactor(plan): fail closed when open Plan observation is unavailable

--- editor-tui-app/HKernel/Editor/TUI/Plan/Workspace.hs
+++ editor-tui-app/HKernel/Editor/TUI/Plan/Workspace.hs
@@ -19,6 +19,7 @@ import HKernel.Editor.TUI.Model
   ( AppContext(..)
   , AppEvent
   , Name(..)
+  , contextOpenPlanObservation
   , contextPlanListL
   )
 import HKernel.Editor.TUI.Scroll qualified as Scroll
@@ -53,44 +54,67 @@ data WorkspaceAction
   deriving (Eq, Show)
 
 drawWorkspace :: AppContext -> Widget Name
-drawWorkspace context =
-  vBox
-    [ borderWithLabel (str "Open Plans (plan.journal)")
-        (vLimit 18
-          (L.renderList renderPlanItem True (contextPlanList context)))
-    , borderWithLabel (str "Selected Plan")
-        (padAll 1 (renderSelectedPlan context))
-    , strWrap "[j/k/Arrows/wheel] Move   [Enter/C] Complete & Advance   [A] Add   [E] Edit"
-    , strWrap "[X] Cancel   [R] Replace"
-    ]
+drawWorkspace context = case contextOpenPlanObservation context of
+  Left errors ->
+    vBox
+      [ borderWithLabel (str "Open Plans (plan.journal)")
+          (padAll 1
+            (withAttr (attrName "warning")
+              (vBox
+                [ strWrap "Open Plan observation is unavailable for this Household observation."
+                , txtWrap ("Reason: " <> T.pack (show errors))
+                , strWrap "Existing Plan mutation targets are hidden rather than treated as an empty set."
+                ])))
+      , strWrap "[A] Add Plan   Existing Plan Complete/Edit/Cancel/Replace unavailable"
+      ]
+  Right _ ->
+    vBox
+      [ borderWithLabel (str "Open Plans (plan.journal)")
+          (vLimit 18
+            (L.renderList renderPlanItem True (contextPlanList context)))
+      , borderWithLabel (str "Selected Plan")
+          (padAll 1 (renderSelectedPlan context))
+      , strWrap "[j/k/Arrows/wheel] Move   [Enter/C] Complete & Advance   [A] Add   [E] Edit"
+      , strWrap "[X] Cancel   [R] Replace"
+      ]
 
 handleWorkspaceEvent
   :: BrickEvent Name AppEvent
   -> EventM Name AppContext WorkspaceAction
-handleWorkspaceEvent event = case Scroll.listWheelEvent PlanList event of
-  Just wheelEvent -> do
-    zoom contextPlanListL (L.handleListEvent wheelEvent)
-    pure MaintainContext
-  Nothing -> case event of
-    MouseDown PlanList V.BLeft _ (Location (_, row)) -> do
-      zoom contextPlanListL (modify (L.listMoveTo row))
-      pure MaintainContext
-    VtyEvent (V.EvKey (V.KChar 'a') []) -> pure OpenAdd
-    VtyEvent (V.EvKey (V.KChar 'A') []) -> pure OpenAdd
-    VtyEvent (V.EvKey (V.KChar 'e') []) -> pure OpenEdit
-    VtyEvent (V.EvKey (V.KChar 'E') []) -> pure OpenEdit
-    VtyEvent (V.EvKey (V.KChar 'x') []) -> pure OpenCancel
-    VtyEvent (V.EvKey (V.KChar 'X') []) -> pure OpenCancel
-    VtyEvent (V.EvKey (V.KChar 'r') []) -> pure OpenReplace
-    VtyEvent (V.EvKey (V.KChar 'R') []) -> pure OpenReplace
-    VtyEvent (V.EvKey V.KEnter []) -> pure OpenCompletion
-    VtyEvent (V.EvKey (V.KChar 'c') []) -> pure OpenCompletion
-    VtyEvent (V.EvKey (V.KChar 'C') []) -> pure OpenCompletion
-    VtyEvent (V.EvKey vtyKey vtyMods) -> do
-      zoom contextPlanListL
-        (L.handleListEventVi L.handleListEvent (V.EvKey vtyKey vtyMods))
-      pure MaintainContext
-    _ -> pure MaintainContext
+handleWorkspaceEvent event = do
+  context <- get
+  case contextOpenPlanObservation context of
+    Left _ -> handleUnavailable event
+    Right _ -> handleAvailable event
+  where
+    handleUnavailable currentEvent = case currentEvent of
+      VtyEvent (V.EvKey (V.KChar 'a') []) -> pure OpenAdd
+      VtyEvent (V.EvKey (V.KChar 'A') []) -> pure OpenAdd
+      _ -> pure MaintainContext
+    handleAvailable currentEvent = case Scroll.listWheelEvent PlanList currentEvent of
+      Just wheelEvent -> do
+        zoom contextPlanListL (L.handleListEvent wheelEvent)
+        pure MaintainContext
+      Nothing -> case currentEvent of
+        MouseDown PlanList V.BLeft _ (Location (_, row)) -> do
+          zoom contextPlanListL (modify (L.listMoveTo row))
+          pure MaintainContext
+        VtyEvent (V.EvKey (V.KChar 'a') []) -> pure OpenAdd
+        VtyEvent (V.EvKey (V.KChar 'A') []) -> pure OpenAdd
+        VtyEvent (V.EvKey (V.KChar 'e') []) -> pure OpenEdit
+        VtyEvent (V.EvKey (V.KChar 'E') []) -> pure OpenEdit
+        VtyEvent (V.EvKey (V.KChar 'x') []) -> pure OpenCancel
+        VtyEvent (V.EvKey (V.KChar 'X') []) -> pure OpenCancel
+        VtyEvent (V.EvKey (V.KChar 'r') []) -> pure OpenReplace
+        VtyEvent (V.EvKey (V.KChar 'R') []) -> pure OpenReplace
+        VtyEvent (V.EvKey V.KEnter []) -> pure OpenCompletion
+        VtyEvent (V.EvKey (V.KChar 'c') []) -> pure OpenCompletion
+        VtyEvent (V.EvKey (V.KChar 'C') []) -> pure OpenCompletion
+        VtyEvent (V.EvKey vtyKey vtyMods) -> do
+          zoom contextPlanListL
+            (L.handleListEventVi L.handleListEvent (V.EvKey vtyKey vtyMods))
+          pure MaintainContext
+        _ -> pure MaintainContext
 
 renderPlanItem :: Bool -> IdentifiedPlanTransaction -> Widget Name
 renderPlanItem selected identified
